a43d972151d245672f8d6d5678e164479164e5c2
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-03-23 22:37:22 +0200

    added current weather integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,7 +65,100 @@ searchField.addEventListener("input", function () {
 					searchResult.querySelector("[data-search-list]").appendChild(searchItem);
 					items.push(searchItem.querySelector("[data-search-toggler]"));
 				}
+
+				addEventOnElements(items, "click", function () {
+					toggleSearch();
+					searchResult.classList.remove("active");
+				})
 			});
 		}, searchTimeoutDuration)
 	}
-})
+});
+
+
+const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector("[data-current-location-btn]");
+const errorContent = document.querySelector("[data-error-content]");
+
+// Рендер всей информации о погоде в html
+export const updateWeather = function (lat, lon) {
+	// loading.style.display = "grid";
+
+	container.style.overflowY = "hidden";
+	container.classList.contains("fade-in") ?? container.classList.remove("fade-in");
+
+	errorContent.style.display = "none";
+
+	const currentWeatherSection = document.querySelector("[data-current-weather]");
+	const highlightSection = document.querySelector("[data-highlights]");
+	const hourlySection = document.querySelector("[data-hourly-forecast]");
+	const forecastSection = document.querySelector("[data-5-day-forecast]");
+
+	currentWeatherSection.innerHTML = "";
+	highlightSection.innerHTML = "";
+	hourlySection.innerHTML = "";
+	forecastSection.innerHTML = "";
+
+	if (window.location.hash === "#/current-location") {
+		currentLocationBtn.setAttribute("disabled", "");
+	} else { 
+		currentLocationBtn.removeAttribute("disabled");
+	}
+
+	// Секция текущей погоды
+	fetchData(url.currentWeather(lat, lon), function (currentWeather) {
+		const {
+			weather,
+			dt: dateUnix,
+			sys: { 
+				sunrise: sunriseUnixUTC, 
+				sunset: sunsetUnixUTC 
+			},
+			main: {
+				temp, feels_like, pressure, humidity
+			},
+			timezone
+		} = currentWeather;
+		const [{ description, icon }] = weather;
+
+		const card = document.createElement("div");
+		card.classList.add("card", "card-lg", "current-weather-card");
+
+		card.innerHTML = `
+			<h2 class="title-2 card-title">Now</h2>
+
+			<div class="wrapper">
+				<p class="heading">${parseInt(temp)}°<sup>c</sup></p>
+
+				<img src="./assets/images/weather_icons/${icon}.png" width="64" height="64" alt="${description}" class="weather-icon">
+			</div>
+
+			<p class="body-3">${description}</p>
+
+			<ul class="meta-list">
+				<li class="meta-item">
+					<span class="m-icon">calendar_today</span>
+
+					<p class="title-3 meta-text">${module.getDate(dateUnix, timezone)}</p>
+				</li>
+
+				<li class="meta-item">
+					<span class="m-icon">location_on</span>
+
+					<p class="title-3 meta-text" data-location></p>
+				</li>
+			</ul>
+		`;
+
+		fetchData(url.reverseGeo(lat, lon), function([{ name, country }]) {
+			card.querySelector("[data-location]").innerHTML = `${name}, ${country}`;
+		});
+
+		currentWeatherSection.appendChild(card);
+	});
+}
+
+export const error404 = function () {
+
+}
